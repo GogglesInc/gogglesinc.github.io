03ccf660e77eef1c1b3b6da19052098006a4c88c
Goggles <ngkhoa0075@gmail.com>
2022-11-21 21:20:57 +0700

    v0.5 Launch

--- public/javascripts/mainpagescript.js
+++ public/javascripts/mainpagescript.js
@@ -1,13 +1,9 @@
 const upperbar = document.querySelector('.upperbar')
 const getstartbtn = document.querySelector('#getstart')
-getstartbtn.addEventListener('click', () => {
-  window.scrollTo({
-    top: 640,
-    behavior: 'smooth'
-  })
-})
+// const disclaimerbtn = document.querySelector('#disclaimer')
+// const disclaimerscreen = document.querySelector('header')
 
-window.addEventListener('scroll', eventscroll = () => {
+window.addEventListener('scroll', () => {
   const scrollTop = window.pageYOffset !== undefined ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop
   let bar
   if (scrollTop >= 640) {
@@ -18,4 +14,30 @@ window.addEventListener('scroll', eventscroll = () => {
   } else {
     upperbar.classList.remove('barafter')
   }
+})
+
+getstartbtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 640,
+    behavior: 'smooth'
+  })
+})
+
+// disclaimerbtn.addEventListener('click', () => {
+//   disclaimerscreen.style.display = 'none'
+// })
+
+const services = document.querySelectorAll('.grid-child')
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    entry.target.classList.toggle('show', entry.isIntersecting)
+    if (entry.isIntersecting) observer.unobserve(entry.target)
+  })
+}, {
+  threshold: 0.3
+})
+
+services.forEach(service => {
+  observer.observe(service)
 })
