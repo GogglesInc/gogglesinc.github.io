6d5ed0762a3b6f4ac773f68f5d1728c6809e9d7e
gogglesgogs <ngkhoa0075@gmail.com>
2024-10-01 06:13:26 +0700

    Remove old code

--- routes/github.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function (req, res, next) {
-	res.redirect(302, 'https://github.com/someguy403/Responsive-Web-Design');
-});
-
-module.exports = router;
--- routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', function (req, res, next) {
-	res.redirect(302, '/');
-});
-
-module.exports = router;
