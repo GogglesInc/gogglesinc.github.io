01efb8c3b4be7da3edab170ce83995def56caee9
gogglesgogs <ngkhoa0075@gmail.com>
2024-10-18 19:15:21 +0700

    Update Lighthouse CI

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+import tailwindcssPlugin from "prettier-plugin-tailwindcss";
+
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: [tailwindcssPlugin],
+};
+
+export default config;
