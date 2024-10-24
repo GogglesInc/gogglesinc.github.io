33bb171356c112de5eefedc1831392a5d0e43f53
gogglesgogs <gogglesgogs@users.noreply.github.com>
2024-10-24 13:03:14 +0000

    chore: fix linting issues

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
   build: {},
   server: {
-    port: 8000,
-  },
-});
+    port: 8000
+  }
+})
