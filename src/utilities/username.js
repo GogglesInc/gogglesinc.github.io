8157e56e0d0e1f419ce6189bea867c82f27c0dec
gogglesgogs <gogglesgogs@users.noreply.github.com>
2024-10-26 05:26:00 +0000

    chore: fix linting issues

--- src/utilities/username.js
+++ src/utilities/username.js
@@ -1,17 +1,17 @@
-export default function username(name) {
+export default function username (name) {
   let username =
-    "@" +
+    '@' +
     name
       .toLowerCase()
-      .replace(/[^\w ]+/g, "")
-      .replace(/ +/g, "")
-      .replace(/_+/g, "");
+      .replace(/[^\w ]+/g, '')
+      .replace(/ +/g, '')
+      .replace(/_+/g, '')
 
-  let lastChar = username.slice(-1);
+  const lastChar = username.slice(-1)
 
-  if (lastChar === "_") {
-    username = username.substring(0, username.length - 1);
+  if (lastChar === '_') {
+    username = username.substring(0, username.length - 1)
   }
 
-  return username;
+  return username
 }
